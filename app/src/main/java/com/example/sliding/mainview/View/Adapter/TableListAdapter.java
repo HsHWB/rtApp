a71ece89a3627f20939c7166e9c9b0a7836a1218
HsHWB <Hswbinforgithub@gmail.com>
2016-02-03 19:33:29 +0800

    #okhttp与后台交互

--- app/src/main/java/com/example/sliding/mainview/View/Adapter/TableListAdapter.java
+++ app/src/main/java/com/example/sliding/mainview/View/Adapter/TableListAdapter.java
@@ -21,7 +21,22 @@ import com.example.sliding.mainview.Activity.OrderAcitivity;
 import com.example.sliding.mainview.Beans.MenuItem;
 import com.example.sliding.mainview.R;
 import com.example.sliding.mainview.Utils.WindowsUtils;
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -90,10 +105,55 @@ public class TableListAdapter extends BaseAdapter implements AdapterView.OnItemC
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        System.out.println("click position == "+position);
+        System.out.println("click position == " + position);
         Intent intent = new Intent(mContext, OrderAcitivity.class);
         intent.putExtra("position", position);
         mContext.startActivity(intent);
+//        //创建okHttpClient对象
+//        OkHttpClient mOkHttpClient = new OkHttpClient();
+//        //创建一个Request
+//        Request request = new Request.Builder().url("http://172.17.0.:8080/rt/test/getData").build();
+//        //new call
+//        Call call = mOkHttpClient.newCall(request);
+//        //请求加入调度
+//        call.enqueue(new Callback()
+//        {
+//            @Override
+//            public void onFailure(Request request, IOException e)
+//            {
+//                System.out.println(request.body());
+//            }
+//
+//            @Override
+//            public void onResponse(final Response response) throws IOException
+//            {
+//                String htmlStr =  response.body().string();
+//                System.out.println(htmlStr);
+//            }
+//        });
+
+//        new Thread() {
+//            @Override
+//            public void run() {
+//                HttpGet getMethod = new HttpGet("http://172.17.0.:8080/rt/test/getData");
+//
+//                HttpClient httpClient = new DefaultHttpClient();
+//
+//                try {
+//                    HttpResponse response = httpClient.execute(getMethod); //发起GET请求
+//                    System.out.println(EntityUtils.toString(response.getEntity(), "utf-8"));
+//
+//                } catch (ClientProtocolException e) {
+//                    // TODO Auto-generated catch block
+//                    e.printStackTrace();
+//                } catch (IOException e) {
+//                    // TODO Auto-generated catch block
+//                    e.printStackTrace();
+//                }
+//            }
+//        }.start();
+
+
     }
 
     class ViewHolder{
