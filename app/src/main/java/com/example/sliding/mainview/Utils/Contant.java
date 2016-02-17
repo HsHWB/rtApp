cb7319463ba2248bbff0c56129fd9163eadebdf7
HsHWB <Hswbinforgithub@gmail.com>
2016-02-17 20:52:22 +0800

    #界面改动

--- app/src/main/java/com/example/sliding/mainview/Utils/Contant.java
+++ app/src/main/java/com/example/sliding/mainview/Utils/Contant.java
@@ -4,10 +4,5 @@ package com.example.sliding.mainview.Utils;
  * Created by huehn on 16/2/12.
  */
 public class Contant {
-    public static String GET_EMPTY_TABLE = "http://www.hswbin.com:8080/rtService/test/getTableByState?tableState=%d";
-    public static String GET_NOTED_TABLE = "http://www.hswbin.com:8080/rtService/test/getTableByState?tableState=%d";
-    public static String ORDER = "http://www.hswbin.com:8080/rtService/test/order";
-    public static String GET_TABLES = "http://www.hswbin.com:8080/rtService/test/getData?table=%s";
-    public static String GET_ITEMS = "http://www.hswbin.com:8080/rtService/test/returnItem?idtable=%d";
-    public static String PAY = "http://www.hswbin.com:8080/rtService/test/closeTable?tableId=%d";
+    
 }
